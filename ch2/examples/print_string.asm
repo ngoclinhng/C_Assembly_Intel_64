ccd89ad9990f0b99b7860c5ba0b61d275674b302
Linh Ng <mail2ngoclinh@gmail.com>
2017-11-18 22:12:48 +0700

    Implement print_string func

---
+++ ch2/examples/print_string.asm
@@ -0,0 +1,70 @@
+; nasm -f macho64 print_string.asm
+; ld -macosx_version_min 10.7.0 -lSystem -o print_string print_string.o
+; ./print_string
+
+global start
+
+section .data
+test_string:	db	"Hello, world", 0
+
+section .text
+; strlen
+; Accepts a pointer to a null-terminated string and outputs
+; its length
+strlen:
+	; Since the return value will be stored in rax
+	; and we also use rax as index into string, we need to zero out it first
+	xor rax, rax			; equivalent to mov rax, 0 but less expensive
+
+.iterate:
+	; Is the current char null?
+	cmp byte [rdi + rax], 0
+
+	; If it is null, go to .end
+	je .end
+
+	; else, increment rax and go to the next interation
+	inc rax
+	jmp .iterate
+.end:
+	ret
+
+; print_string
+; Accepts a pointer to a null-terminated string and outputs it
+; to the stdout
+print_string:
+	; first, call strlen to get the length of the input string.
+	; but a call to strlen could posibly change rdi, we need to save it
+	; first
+	push rdi
+	call strlen
+
+	; at this point, rax contains the length of the input string
+	; we want this to be the third argument to system write
+	; function
+	mov rdx, rax
+
+	; pop stack and store value into rsi which is the 2nd
+	; argument to system write routine
+	pop rsi
+
+	mov rax, 0x2000004		; write syscall
+	mov rdi, 1			; first argument is stdout
+
+	; invoke system write(rdi, rsi, rdx)
+	; where rdi is stdout, rsi is char *, rdx is
+	; the number of bytes to be written
+	syscall
+
+	; exit
+	ret
+	
+; main
+start:
+	mov rdi, test_string
+	call print_string
+
+	; exit
+	mov rax, 0x2000001		; exit syscall
+	xor rdi, rdi			; status code is 0
+	syscall
